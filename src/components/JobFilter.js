4ea8a0c3e430f9db818b647374afb849d94ab98f
ak708 <71438458+ak708@users.noreply.github.com>
2024-03-16 15:31:07 +0530

    hero,job,api complete

--- src/components/JobFilter.js
+++ src/components/JobFilter.js
@@ -1,106 +1,106 @@
 import React from "react";
 
 const JobFilter = () => {
-	return (
-		<div className="max-w-[350px] flex flex-col gap-4">
-			<div className="lg:flex items-center gap-3">
-				<input
-					type="text"
-					placeholder="Search for a job"
-					className="bg-[#1E1E1E] text-white px-3 py-2 rounded-lg max-w-[150px]"
-				/>
-				<button className="bg-[#2563EB] text-white px-4 py-2 rounded-lg">
-					Search
-				</button>
-			</div>
-			<div>
-				Job Type
-				<div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						Sales/Marketing
-					</div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						Engineering
-					</div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						IT
-					</div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						Product
-					</div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						Design
-					</div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						Business Dev
-					</div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						Others
-					</div>
-				</div>
-			</div>
-			<div>
-				Location
-				<div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						United States
-					</div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						Hong Kong
-					</div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						Indonesia
-					</div>
-				</div>
-			</div>
-			<div>
-				Skills
-				<div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						Frontend
-					</div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						Backend
-					</div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						C++
-					</div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						Javascript
-					</div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						Python
-					</div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						Ruby
-					</div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						Security
-					</div>{" "}
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						PHP
-					</div>
-				</div>
-			</div>
-			<div>
-				Employement Type
-				<div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						Full-Time
-					</div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						Part-Time
-					</div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						Contract
-					</div>
-					<div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
-						Freelance
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="max-w-[350px] flex flex-col gap-4">
+      <div className="lg:flex items-center gap-3">
+        <input
+          type="text"
+          placeholder="Search for a job"
+          className="bg-[#1E1E1E] text-white px-3 py-2 rounded-lg max-w-[150px]"
+        />
+        <button className="bg-[#22c55e] text-black px-4 py-2 rounded-lg">
+          Search
+        </button>
+      </div>
+      <div>
+        Job Type
+        <div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            Sales/Marketing
+          </div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            Engineering
+          </div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            IT
+          </div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            Product
+          </div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            Design
+          </div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            Business Dev
+          </div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            Others
+          </div>
+        </div>
+      </div>
+      <div>
+        Location
+        <div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            United States
+          </div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            Hong Kong
+          </div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            Indonesia
+          </div>
+        </div>
+      </div>
+      <div>
+        Skills
+        <div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            Frontend
+          </div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            Backend
+          </div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            C++
+          </div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            Javascript
+          </div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            Python
+          </div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            Ruby
+          </div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            Security
+          </div>{" "}
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            PHP
+          </div>
+        </div>
+      </div>
+      <div>
+        Employement Type
+        <div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            Full-Time
+          </div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            Part-Time
+          </div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            Contract
+          </div>
+          <div className="p-2 border-white border rounded-lg w-fit inline-block mr-2 mt-2 cursor-pointer">
+            Freelance
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default JobFilter;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,31 +2,31 @@ import React from "react";
 import Link from "next/link";
 
 const Navbar = () => {
-	return (
-		<div className="text-[#ffffff] p-5 flex flex-row  justify-center absolute top-0 left-0 bg-black z-10 w-screen">
-			<div className="p-2 w-[80%] flex flex-row items-center justify-around bg-gradient-to-r ">
-				<h1 className="text-2xl font-bold ">
-					<Link href="/">Cracked</Link>
-				</h1>
-				<ul className="flex flex-row gap-10 flex-wrap">
-					<li className="hover:text-green-500 hover:font-bold rounded-full p-1 backdrop-blur-lg transition">
-						<Link href="/jobs" className="cursor-pointer">
-							Jobs
-						</Link>
-					</li>
-					<li className="hover:text-green-500 hover:font-bold rounded-full p-1 backdrop-blur-lg transition">
-						Companies
-					</li>
-					<li className="hover:text-green-500 hover:font-bold rounded-full p-1 backdrop-blur-lg transition">
-						About
-					</li>
-				</ul>
-				<button className="hover:bg-green-500 p-2 rounded-md hover:text-white transition">
-					Sign In
-				</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="text-[#ffffff] p-5 flex flex-row  justify-center absolute top-0 left-0 bg-black z-10 w-screen">
+      <div className="p-2 w-[80%] flex flex-row items-center justify-around bg-gradient-to-r ">
+        <h1 className="text-2xl font-bold ">
+          <Link href="/">RTH.ai</Link>
+        </h1>
+        <ul className="flex flex-row gap-10 flex-wrap">
+          <li className="hover:text-green-500 hover:font-bold rounded-full p-1 backdrop-blur-lg transition">
+            <Link href="/jobs" className="cursor-pointer">
+              Jobs
+            </Link>
+          </li>
+          <li className="hover:text-green-500 hover:font-bold rounded-full p-1 backdrop-blur-lg transition">
+            Community
+          </li>
+          <li className="hover:text-green-500 hover:font-bold rounded-full p-1 backdrop-blur-lg transition">
+            About
+          </li>
+        </ul>
+        <button className="hover:bg-green-500 p-2 rounded-md hover:text-white transition">
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
